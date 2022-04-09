e892321348dd21d945886472a7ac3880ee943570
Deletking <deletkingdelet@gmail.com>
2022-04-09 09:45:25 -0300

    created module store

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
+import { StoreModule } from 'src/modules/store/store.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://Deletking:dq8c0yuEEnzE9nI4@cluster0.xdjbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
     BackofficeModule,
+    StoreModule,
   ],
   controllers: [],
   providers: [],

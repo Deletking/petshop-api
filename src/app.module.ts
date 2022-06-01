a36964b82c1db2c5999b0a254d107ffc5052417a
Deletking <deletkingdelet@gmail.com>
2022-05-31 21:42:03 -0300

    create store and set configuration database in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
 import { StoreModule } from 'src/modules/store/store.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://Deletking:dq8c0yuEEnzE9nI4@cluster0.xdjbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'mysql',
+      database: 'nestjs-balta',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     BackofficeModule,
     StoreModule,
   ],
---
+++ src/modules/store/entities/product.entity.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 80 })
+  title: string;
+
+  @Column('text')
+  description: string;
+}

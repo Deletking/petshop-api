041b0c1312a74d967c6b95e5a0522d0fa8987724
Paulo Hericles <paulo.neves.rgr@segurosunimed.com.br>
2023-02-22 19:36:15 -0300

    create custom authguard

--- src/modules/backoffice/controllers/account.controller.ts
+++ src/modules/backoffice/controllers/account.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get, Post, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
+import { JwtAuthGuard } from "src/shared/guards/auth.guard";
 import { AuthService } from "src/shared/services/auth.service";
 @Controller('v1/accounts')
 export class AccountController {
@@ -13,7 +13,7 @@ export class AccountController {
   }
 
   @Get('')
-  @UseGuards(AuthGuard())
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return [];
   }
---
+++ src/shared/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext, Injectable, UnauthorizedException} from '@nestjs/common';
+
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard() {
+  canActivate(context: ExecutionContext) {
+      return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info) {
+    if(err || !user) {
+      throw err || new UnauthorizedException();
+    }
+
+    return user
+  }
+}
